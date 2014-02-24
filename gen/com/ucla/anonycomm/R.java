0d69b85d6e5871f0fb1543b8b6d330c458367d6a
qianxuweiren <ao@Aos-MacBook-Air.local>
2014-02-23 21:29:35 -0800

    add icons for settings and receive_messages

--- gen/com/ucla/anonycomm/R.java
+++ gen/com/ucla/anonycomm/R.java
@@ -23,6 +23,8 @@ public final class R {
     public static final class drawable {
         public static final int email_icon=0x7f020000;
         public static final int ic_launcher=0x7f020001;
+        public static final int ic_menu_icon_menu_settings=0x7f020002;
+        public static final int ic_menu_rec=0x7f020003;
     }
     public static final class id {
         public static final int action_settings=0x7f080005;
