99572434266368be1a36fa17bd16625df6d21e3e
taxidriverhk <taxidriverhk@gmail.com>
2014-02-26 03:35:38 -0800

    Added functionality of getting a photo and display it

--- src/com/ucla/anonycomm/DisplayMessageActivity.java
+++ src/com/ucla/anonycomm/DisplayMessageActivity.java
@@ -1,33 +1,32 @@
 package com.ucla.anonycomm;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
-
+import java.io.IOException;
 import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
+import org.apache.http.HttpVersion;
 import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicNameValuePair;
-
-import org.apache.http.entity.mime.HttpMultipartMode;
+import org.apache.http.params.CoreProtocolPNames;
 import org.apache.http.entity.mime.MultipartEntity;
 import org.apache.http.entity.mime.content.FileBody;
 import org.apache.http.entity.mime.content.StringBody;
 
-import com.ucla.anonycomm.R;
 import android.annotation.TargetApi;
+import android.app.ActionBar.LayoutParams;
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v4.app.NavUtils;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class DisplayMessageActivity extends Activity {
 
@@ -40,15 +39,17 @@ public class DisplayMessageActivity extends Activity {
 		}
 		Intent intent = getIntent();
 		String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
-		
-		// Create the text view
-		TextView textView = new TextView(this);
-		textView.setTextSize(40);
-		textView.setText(message);
-		
-		new HTTPConn().execute();
-		
-		setContentView(textView);
+
+		// Create the image view
+		ImageView imageView = new ImageView(this);
+		LayoutParams lp = new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+		String path = message;
+		Bitmap image = BitmapFactory.decodeFile(path);
+		imageView.setImageBitmap(image);
+
+		// new HTTPConn().execute();
+
+		setContentView(imageView, lp);
 	}
 
 	/**
@@ -61,46 +62,35 @@ public class DisplayMessageActivity extends Activity {
 		}
 	}
 
-	 private class HTTPConn extends AsyncTask<Void, Void, Void> {
-	        @Override
-	        protected Void doInBackground(Void... arg) {
-	              
-	        	HttpClient httpclient = new DefaultHttpClient();
-	    	    HttpPost httppost = new HttpPost("http://108.168.239.90:8080/session/send");
-	    		/* New code */
-	    	    try {
-	    	    	MultipartEntity entity = new MultipartEntity(HttpMultipartMode.BROWSER_COMPATIBLE);
-	    	    	// Change 'path' to get input from box instead of hard-coded
-	    	    	NameValuePair p = new BasicNameValuePair("image", "ic_launcher-web.png");
-	    	    	entity.addPart(p.getName(), new FileBody(new File(p.getValue())));
-	    	    	httppost.setEntity(entity);
-	    	    	
-	    	    	HttpResponse response = httpclient.execute(httppost);
-	    	    } catch (Exception e){}
-	    	    /* New code */
-	    	    
-	    	    /* Old Code
-	    	    try {
-	    	        // Add your data
-	    	        List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(3);
-	    	        nameValuePairs.add(new BasicNameValuePair("offset", "0"));
-	    	        nameValuePairs.add(new BasicNameValuePair("count", "1"));
-	    	        nameValuePairs.add(new BasicNameValuePair("wait", "false"));
-	    	        httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
-
-	    	        // Execute HTTP Post Request
-	    	        HttpResponse response = httpclient.execute(httppost);
-	    	    } catch (Exception e) {
-	    	    }
-	    	    return null;
-	    	    */
-	    	    return null;
+	private class HTTPConn extends AsyncTask<Void, Void, Void> {
+	    @Override
+	    protected Void doInBackground(Void... arg) {
+	    	HttpClient httpclient = new DefaultHttpClient();
+	        httpclient.getParams().setParameter(CoreProtocolPNames.PROTOCOL_VERSION, HttpVersion.HTTP_1_1);
+	        HttpPost httppost = new HttpPost("http://108.168.239.90:8080/session/send");
+	        
+	        try {
+	        	// Get the image from the path
+	        	EditText text = (EditText) findViewById(R.id.edit_message);
+		        File file = new File(text.getText().toString());
+	        	// Add your data
+		        MultipartEntity entity = new MultipartEntity();
+	        	entity.addPart("offset", new StringBody("0"));
+	        	entity.addPart("count", new StringBody("1"));
+	        	entity.addPart("wait", new StringBody("false"));
+	        	entity.addPart("image", new FileBody(file, "image/bmp"));
+	        	
+    	        httppost.setEntity(entity);
+    	        HttpResponse response = httpclient.execute(httppost);
+	        } catch (IOException e){
+	        	e.printStackTrace();
 	        }
-	        // onPostExecute displays the results of the AsyncTask.
-	        @Override
-	        protected void onPostExecute(Void res) {
-	       }
+	        return null;
 	    }
+	    // onPostExecute displays the results of the AsyncTask.
+        @Override
+        protected void onPostExecute(Void res) {}
+	}
 	 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
--- src/com/ucla/anonycomm/MainActivity.java
+++ src/com/ucla/anonycomm/MainActivity.java
@@ -1,11 +1,16 @@
 package com.ucla.anonycomm;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -19,7 +24,6 @@ public class MainActivity extends Activity {
 
     }
 
-    
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle presses on the action bar items
@@ -46,7 +50,6 @@ public class MainActivity extends Activity {
     	}
     }
 
-    
     public void sendMessage(View view) {
     	Intent intent = new Intent(this, DisplayMessageActivity.class);
     	EditText editText = (EditText) findViewById(R.id.edit_message);
@@ -59,6 +62,41 @@ public class MainActivity extends Activity {
     	Intent intent = new Intent(this, ReceiveMessage.class);
     	startActivity(intent);
     }
+    
+    // Added code for getting photo and display it
+    public void selectPhoto(View view) {
+    	InputMethodManager imm = (InputMethodManager)getSystemService(
+    		      Context.INPUT_METHOD_SERVICE);
+    	imm.hideSoftInputFromWindow(view.getWindowToken(), 2);
+    	Intent intent = new Intent();
+    	intent.setType("image/bmp");
+    	intent.setAction(Intent.ACTION_GET_CONTENT);
+    	startActivityForResult(Intent.createChooser(intent, "Select Picture"), 1);
+    }
+    
+    private String getRealPathFromURI(Uri contentUri) {
+        String[] proj = { MediaStore.Images.Media.DATA };
+        Cursor cursor = managedQuery(contentUri, proj, null, null, null);
+        int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        cursor.moveToFirst();
+        return cursor.getString(column_index);
+    }
+    
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    	switch (requestCode) {
+	    	case 1:
+	    	{
+	    		Uri udata = data.getData();
+	    		String path = getRealPathFromURI(udata);
+	    		EditText text = (EditText) findViewById(R.id.edit_message);
+	    		text.setText(path);
+	    	}
+	    	break;
+    	}
+    	super.onActivityResult(requestCode, resultCode, data);
+    }
+	// Added code for getting photo and display it
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
